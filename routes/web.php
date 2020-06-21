53000dca1aa47e5777dafa41822f387d41bdc36b
unknown <mr.thetnaing94@gmail.com>
2020-06-21 22:19:21 +0630

    first commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::group(['prefix'=>'admin', 'middleware'=>'auth', 'namespace'=>'admin'], function(){
+    Route::get('dashboard', 'DashboardController@index')->name('admin.dashboard');
+    Route::resource('slider', 'SliderController');
+});
